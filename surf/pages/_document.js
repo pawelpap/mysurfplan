1afb2dadffd74dcc9eded4ef0cc52956c2a3a837
pawelpap <pawelpap@me.com>
2025-08-08 19:38:04 +0100

    Update _document.js

--- surf/pages/_document.js
+++ surf/pages/_document.js
@@ -1,1 +1,16 @@
-console.log('_document.js placeholder - paste document code here')
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script src="https://cdn.tailwindcss.com" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body className="bg-gray-50 text-gray-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
