23bdcc385a8c54025d80a24a93eb87bea6d20857
Pawel Paplinski <pawel@Prime-Radiant.local>
2026-09-06 16:15:29 +0100

    Fix mobile browser viewport scaling

--- surf/pages/_app.js
+++ surf/pages/_app.js
@@ -1,13 +1,22 @@
+import Head from "next/head";
 import "../styles/themes.css";
 import "../styles/app.css";
 import { ThemeProvider } from "../components/theme";
 import { SpotLocationProvider } from "../components/spot-select";
 export default function MyWavePlan({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <SpotLocationProvider>
-        <Component {...pageProps} />
-      </SpotLocationProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </Head>
+      <ThemeProvider>
+        <SpotLocationProvider>
+          <Component {...pageProps} />
+        </SpotLocationProvider>
+      </ThemeProvider>
+    </>
   );
 }
